810f17f5a8a3ed56f2fc89425c85310caf69b59a
aglezespinosamx <155202393+aglezespinosamx@users.noreply.github.com>
2023-12-30 10:32:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ import textwrap
 os.environ["OPENAI_API_KEY"] = '23aHHyoox2XgmbFO'
 
 # Leer los PDFs
-# pdf = SimpleDirectoryReader('datos').load_data()
-pdf = PyPDF2.PdfReader('ResumenTelefonica.pdf')
+pdf = SimpleDirectoryReader('datos').load_data()
+# pdf = PyPDF2.PdfReader('ResumenTelefonica.pdf')
 
 with st.sidebar:
     st.title('🤖💬 Chatbot de Angel Gonzalez E')
