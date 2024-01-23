5731bddd12cfcb5e2d7d486c30991ed604569040
aglezespinosamx <155202393+aglezespinosamx@users.noreply.github.com>
2024-01-22 21:30:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import openai
 from llama_index import SimpleDirectoryReader
 
 st.set_page_config(page_title="Chat de Datos Financieros", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-openai.api_key = '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'
+openai.api_key = st.secrets.openai_key
 # st.secrets.openai_key
 st.title("Chatea con documentos 💬")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
