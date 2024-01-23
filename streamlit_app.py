5200b41c96bfbd3c42082e916d19a3a0031d0950
aglezespinosamx <155202393+aglezespinosamx@users.noreply.github.com>
2024-01-23 16:31:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ index = load_data()
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Cual es tu pregunta?"): # Prompt for user input and save to chat history
+if prompt := st.chat_input("¿Cuál es tu pregunta?"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
 for message in st.session_state.messages: # Display the prior chat messages
